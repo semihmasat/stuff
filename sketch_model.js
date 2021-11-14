25066a7b007624091b6af8804e04bfa125fbe3e2
Semih Masat <semihmasat@outlook.com>
2021-11-14 19:35:18 +0300

    update model 2

--- sketch_model.js
+++ sketch_model.js
@@ -21,22 +21,11 @@ function draw(){
   ambientLight(100);
   directionalLight(255,255,255,0,0,-1);
 
-  //modify graphics
-  graphics.background("pink");
-  graphics.textSize(64);
-  graphics.textAlign(CENTER);
-  graphics.text("lööö",100,100);
-  graphics.image(camera, 0, 0, width/2, width * camera.height / camera.width);
-
-  //rotateX(angle);
   rotateX(PI);
   rotateY(angle);
-  //rotateZ(angle * 0.7);
 
-  texture(graphics);
   noStroke();
-
+  texture(camera);
   model(mickey);
-
   angle += 0.01;
 }
